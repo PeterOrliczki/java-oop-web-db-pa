ec10648b4f35f789864586e8c87451cddaf92d41
PeterOrliczki <peterorliczkiprimary@gmail.com>
2019-06-07 11:55:19 +0200

    User registration in doPost

---
+++ src/main/java/com/codecool/web/servlet/RegisterServlet.java
@@ -0,0 +1,47 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.dao.database.DatabaseUserDao;
+import com.codecool.web.model.Role;
+import com.codecool.web.model.User;
+import com.codecool.web.service.PasswordService;
+import com.codecool.web.service.UserService;
+import com.codecool.web.service.simple.SimpleUserService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/register")
+public class RegisterServlet extends AbstractServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            UserDao userDao = new DatabaseUserDao(connection);
+            UserService userService = new SimpleUserService(userDao);
+            PasswordService passwordService = new PasswordService();
+
+            String name = req.getParameter("name");
+            String email = req.getParameter("email");
+            String password = req.getParameter("password");
+            Role role = Role.REGISTERED;
+
+            if (!userService.findIfUserExists(email)) {
+                User user = userService.addUser(name, email, passwordService.getHashedPassword(password), role, 0);
+                req.setAttribute("user", user);
+                sendMessage(resp, HttpServletResponse.SC_OK, user);
+            }
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            sendMessage(resp, HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+        } catch (SQLException ex) {
+            handleSqlError(resp, ex);
+        }
+    }
+
+}
