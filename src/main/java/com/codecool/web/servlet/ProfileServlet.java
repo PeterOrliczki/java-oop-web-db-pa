faff8b2c6aa109b67f861e8f4b4e8d1263602a5c
PeterOrliczki <peterorliczkiprimary@gmail.com>
2019-06-22 20:29:02 +0200

    Registration, profile info showing and editing, sending logout response

--- src/main/java/com/codecool/web/servlet/ProfileServlet.java
+++ src/main/java/com/codecool/web/servlet/ProfileServlet.java
@@ -1,21 +1,45 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dao.UserDao;
+import com.codecool.web.dao.database.DatabaseUserDao;
 import com.codecool.web.model.User;
+import com.codecool.web.service.UserService;
+import com.codecool.web.service.simple.SimpleUserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet("/protected/profile")
-public final class ProfileServlet extends HttpServlet {
+public final class ProfileServlet extends AbstractServlet {
+
+    private final ObjectMapper om = new ObjectMapper();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = (User) req.getSession().getAttribute("user");
-        req.setAttribute("user", user);
-        req.getRequestDispatcher("profile.jsp").forward(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("user");
+        sendMessage(response, HttpServletResponse.SC_OK, user);
+    }
+
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            UserDao userDao = new DatabaseUserDao(connection);
+            UserService userService = new SimpleUserService(userDao);
+
+            User user = om.readValue(req.getInputStream(), User.class);
+
+            userService.updateEmailById(user.getId(), user.getUserEmail());
+            userService.updatePasswordById(user.getId(), user.getUserPassword());
+
+            req.getSession().setAttribute("user", user);
+            sendMessage(resp, HttpServletResponse.SC_OK, "Your data has been updated.");
+        } catch (SQLException exc) {
+            handleSqlError(resp, exc);
+        }
     }
 }
